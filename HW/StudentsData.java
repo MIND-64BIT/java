181ac81298f260e1af1cc0069ab36953b6408e7e
MIND-64BIT <178900363+MIND-64BIT@users.noreply.github.com>
2025-07-11 08:12:10 +0530

    Create StudentsData.java

---
+++ HW/StudentsData.java
@@ -0,0 +1,30 @@
+
+class Student{
+
+  String name;
+  int rollNo;
+
+  Student(String name, int roll){
+      this.name=name;
+      this.rollNo=roll;
+  }
+
+  void displayDetails(){
+      System.out.print("Name: "+this.name+"\nRoll No: "+this.rollNo);
+  }
+
+{
+
+
+
+
+
+public class StudentsData{
+  public static void main( String[] args){
+        Student s1 = new Student("Riya", 1);
+        Student s1 = new Student("Ruhi", 2);
+
+        s1.dispalyDetails()
+        s2.displayDetails()
+  }
+}
